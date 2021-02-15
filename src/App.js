0e366309f921768f9f15307e51d66352bf8d4151
arsdeeennya <kkamazonkk@yahoo.co.jp>
2021-02-16 02:10:29 +0900

    proptypes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from "react";
-
-// 関数コンポーネント
-// function App() {
-//   return (
-//   	<React.Fragment>
-// 		<label htmlFor="bar">bar</label>
-// 		<input type="text" onChange={() => {console.log("I am clicked")}} />
-// 	</React.Fragment>
-//   )
-// }
+import PropTypes from "prop-types";
 
 // クラスコンポーネント
 const App = () => {
-	return <div>Hi!</div>
+	const profiles = [
+		{ name: "taro", age: 10},
+		{ name: "hanako", age: 5},
+		{ name: "NoName"},
+	]
+	return (
+		<div>
+			{
+				profiles.map((profile, index) => {
+					return <User name={profile.name} age={profile.age} key={index}/>
+				})
+			}
+			
+		</div>
+	)
 }
+
+const User = (props) => {
+	return <div>Hi, I am {props.name}, and {props.age} years old" </div>
+}
+
+User.propTypes = {
+	name: PropTypes.string,
+	age: PropTypes.number.isRequired,
+}
+
 export default App;
