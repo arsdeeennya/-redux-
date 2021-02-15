8420ac61b7231a93cb44a7d8bf010292d24e4838
arsdeeennya <kkamazonkk@yahoo.co.jp>
2021-02-16 00:50:03 +0900

    create class component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-function App() {
-  return (
-    <div>
-      <h1>こんちは</h1>
-    </div>
-  );
-}
+import React from "react";
 
+// 関数コンポーネント
+// function App() {
+//   return (
+//   	<React.Fragment>
+// 		<label htmlFor="bar">bar</label>
+// 		<input type="text" onChange={() => {console.log("I am clicked")}} />
+// 	</React.Fragment>
+//   )
+// }
+
+// クラスコンポーネント
+const App = () => {
+	return <div>Hi!</div>
+}
 export default App;
